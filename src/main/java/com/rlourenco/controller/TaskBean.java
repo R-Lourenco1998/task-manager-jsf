f42d760cc1636b8260d8d3326286ba51eb84a773
Luiz Ricardo Lourenço da Silva <46008216+R-Lourenco1998@users.noreply.github.com>
2023-06-11 11:58:52 -0300

    default locale

--- src/main/java/com/rlourenco/controller/TaskBean.java
+++ src/main/java/com/rlourenco/controller/TaskBean.java
@@ -1,16 +1,14 @@
 package com.rlourenco.controller;
 
 import java.io.Serializable;
-
-import javax.faces.bean.ViewScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-
 import com.rlourenco.entity.Task;
 import com.rlourenco.enumerator.PriorityLevelEnum;
 import com.rlourenco.enumerator.ResponsibleEnum;
 import com.rlourenco.enumerator.SituationEnum;
 
-@Named("taskBean")
+@Named()
 @ViewScoped
 public class TaskBean implements Serializable {
 
@@ -18,13 +16,13 @@ public class TaskBean implements Serializable {
 
 	private Task task = new Task();
 
-	public Task getTask() {
-		return task;
-	}
-
 	public void save() {
 		System.out.println("Título: " + task.getTitle() + "Descrição: " + task.getDescription() + "Prioridade: "
 				+ task.getPriority() + "Deadline: " + task.getDeadline() + "Responsável: " + task.getResponsible());
+	}
+
+	public Task getTask() {
+		return task;
 	}
 
 	public PriorityLevelEnum[] getPriorityLevels() {
