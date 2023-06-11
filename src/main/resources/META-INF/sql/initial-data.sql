e96829cc3ec16de50b0f0fe29151b82d9cdb6bf8
Luiz Ricardo Lourenço da Silva <46008216+R-Lourenco1998@users.noreply.github.com>
2023-06-11 13:57:26 -0300

    settings persistance

---
+++ src/main/resources/META-INF/sql/initial-data.sql
@@ -0,0 +1,54 @@
+insert into task (
+        id,
+        deadline,
+        description,
+        priority_level,
+        responsible,
+        situation,
+        title
+    )
+values (
+        1,
+        '2023-06-15',
+        'Gerenciador de Tarefas',
+        'HIGH',
+        'RICARDO',
+        'IN_PROGRESS',
+        'Teste técnico da ESIG'
+    )
+insert into task (
+        id,
+        deadline,
+        description,
+        priority_level,
+        responsible,
+        situation,
+        title
+    )
+values (
+        2,
+        '2023-12-5',
+        'Entrega artigo',
+        'HIGH',
+        'RICARDO',
+        'IN_PROGRESS',
+        'TCC Parte 2'
+    )
+insert into task (
+        id,
+        deadline,
+        description,
+        priority_level,
+        responsible,
+        situation,
+        title
+    )
+values (
+        3,
+        '2023-12-31',
+        'Ficar no shape',
+        'HIGH',
+        'RICARDO',
+        'IN_PROGRESS',
+        'É o emagreces'
+    )
